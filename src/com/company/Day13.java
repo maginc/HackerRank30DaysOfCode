d21f38916d4b4f9ff141b2dee4c272f2f611ca75
maginc <andrisdabest@gmail.com>
2020-02-24 09:03:23 +0100

    day 13 challenge

---
+++ src/com/company/Day13.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.util.*;
+
+/**
+ * @author Andris Magins
+ * @created 24-Feb-20
+ **/
+public class Day13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+
+}
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+// Declare your class here. Do not use the 'public' access modifier.
+// Declare the price instance
+
+class  MyBook extends Book {
+
+    private int price;
+/**
+ *   Class Constructor
+ *
+ *   @param title The book's title.
+ *   @param author The book's author.
+ *   @param price The book's price.
+ **/
+// Write your constructor here
+
+    public MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+
+/**
+ *   Method Name: display
+ *
+ *   Print the title, author, and price in the specified format.
+ **/
+// Write your method here
+
+@Override
+void display() {
+    System.out.println("Title: " + title + '\n' +
+            "Author: " + author + '\n' +
+            "Price: " + price +
+            "");
+
+}
+
+
+    // End class
+
+}
