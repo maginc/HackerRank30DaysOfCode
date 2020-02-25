752f00ae77df5adf7f3069679db36c168fdd7641
maginc <andrisdabest@gmail.com>
2020-02-25 19:08:54 +0100

    day 11 challenge

--- src/com/company/Day11.java
+++ src/com/company/Day11.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -23,5 +24,18 @@ public class Day11 {
         }
 
         scanner.close();
+
+        int sum[] = new int[16];
+        int h = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                sum[h] = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                        + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]
+                        + arr[i+2][j+2];
+                h++;
+            }
+        }
+        Arrays.sort(sum);
+        System.out.println(sum[15]);
     }
 }
