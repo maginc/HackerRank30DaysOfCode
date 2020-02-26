30629965adb9b1d1bffc2e7f3c22c1b30c38f541
maginc <andrisdabest@gmail.com>
2020-02-26 10:02:47 +0100

    day 21 challenge

---
+++ src/com/company/Day21.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Andris Magins
+ * @created 26-Feb-20
+ **/
+public class Day21 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
+}
+
+class Printer <T> {
+
+    /**
+     *    Method Name: printArray
+     *    Print each element of the generic array on a new line. Do not return anything.
+     *    @param A generic array
+     **/
+
+    // Write your code here
+    void printArray(T[] A){
+
+        for(T element: A){
+            System.out.println(element);
+        }
+    }
+
+}
