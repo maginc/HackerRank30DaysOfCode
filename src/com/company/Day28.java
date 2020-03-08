7cb81f61a30ff6b249f54a82dad26b3913b96596
maginc <andrisdabest@gmail.com>
2020-03-08 11:37:51 +0100

    day 28 challenge

---
+++ src/com/company/Day28.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Andris Magins
+ * @created 08-Mar-20
+ **/
+public class Day28 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String emailRegEx = ".+@gmail\\.com$";
+        Pattern pattern = Pattern.compile(emailRegEx);
+        List<String> list = new ArrayList();
+        for (int i = 0; i < num; i++){
+            String name = scanner.next();
+            String email = scanner.next();
+            Matcher matcher = pattern.matcher(email);
+            if (matcher.find()){
+                list.add(name);
+            }
+        }
+        Collections.sort(list);
+        for (String name : list){
+            System.out.println(name);
+        }
+    }
+}
